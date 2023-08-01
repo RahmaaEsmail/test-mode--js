513dd0d62d297b1d57df7ce85ebfbfee693df7db
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-08-01 06:14:19 +0300

    Refactor the code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
+'use strict'
 const viewTestBtn = document.querySelector(".view-test-btn");
 const quizPage = document.querySelector(".quiz-page");
 const homePage = document.querySelector(".home-page");
-const list = document.querySelector("ul")
+const leaderboardPage = document.querySelector(".leaderboard");
+const timerEle = document.querySelector(".timer");
 const nextQuestion = document.querySelector(".next-question");
 const checkAnsBtn = document.querySelector(".check-question");
-const submitBtn = document.querySelector(".submit-btn");
 const scoreEle = document.querySelector(".score");
-let undoStack = [];
-let redoStack = [];
-let dataList = [];
-let ans = '';
-<<<<<<< HEAD
-let time, count, setTimer, isChecked = false, score = 0, currentIndex = 0;
-let isAnswered = false;
-let lastQuestionReached = 0
-=======
-let time, count, setTimer, checked = false, score = 0, currentIndex = 0;
-let isAnswered = false
-let lastQuestionReached = 0
+const submitBtn = document.querySelector(".submit-btn");
 const allUserAnswers = []
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
-
+let dataList = [];
+let time, count, setTimer, isChecked = false, isAnswered = false, lastQuestionReached = 0, score = 0, currentIndex = 0;
+let userAnswer = ''
 
 const showHomePage = () => {
     quizPage.classList.add("hide");
-    document.querySelector(".leaderboard").classList.add("hide")
+    leaderboardPage.classList.add("hide")
     homePage.classList.remove("hide")
 }
 
@@ -39,7 +30,7 @@ showQuizPage()
 
 const showLeaderBoard = (score) => {
     quizPage.classList.add("hide");
-    document.querySelector(".leaderboard").classList.remove("hide")
+    leaderboardPage.classList.remove("hide")
     document.querySelector(".leader-score").innerHTML = score
     return;
 }
@@ -76,17 +67,6 @@ const displayElements = (allQuestions, currentIndex) => {
         return;
     }
 
-<<<<<<< HEAD
-    if (currentIndex < lastQuestionReached) {
-        isAnswered = true
-        removeCheckBtn()
-    } else {
-        isAnswered = false
-        removeNextBtn()
-    }
-
-=======
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
     quesContainer.innerHTML = allQuestions[currentIndex].title;
     let htmlList = `
     <li data-index = "0">${allQuestions[currentIndex].answer_1}</li>
@@ -94,10 +74,11 @@ const displayElements = (allQuestions, currentIndex) => {
     <li data-index = "2">${allQuestions[currentIndex].answer_3}</li>
     <li data-index = "3">${allQuestions[currentIndex].answer_4}</li>`
     list.innerHTML = htmlList;
-    chooseAnswer(allQuestions, list, allQuestions[currentIndex].index)
-<<<<<<< HEAD
-=======
+    chooseAnswer(list, allQuestions[currentIndex].index)
+    displayChoosedElements(allQuestions,list)
+}
 
+const displayChoosedElements = (allQuestions,list) => {
     if (currentIndex < lastQuestionReached) {
         isAnswered = true
         removeCheckBtn()
@@ -114,18 +95,16 @@ const displayElements = (allQuestions, currentIndex) => {
         isAnswered = false
         removeNextBtn()
     }
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
 }
 
-
-const checkChoosedAnswer = (ans, indexOfAnswer, items) => {
-    if (ans.dataset.index == indexOfAnswer) {
+const checkChoosedAnswer = (answer, indexOfAnswer, items) => {
+    if (answer.dataset.index == indexOfAnswer) {
         items.forEach(item => {
             item.classList.remove("wrong-answer")
             item.classList.remove("choosen-answer")
         })
         scoreEle.innerHTML = score;
-        ans.classList.add("correct-answer")
+        answer.classList.add("correct-answer")
         swal("Correct Answer", '', "success")
     }
     else {
@@ -136,63 +115,43 @@ const checkChoosedAnswer = (ans, indexOfAnswer, items) => {
             }
         })
         scoreEle.innerHTML = score;
-        ans.classList.add("wrong-answer")
+        answer.classList.add("wrong-answer")
         swal("Wrong Answer", '', 'error')
     }
 }
 
-const chooseAnswer = (allQuestions, list, indexOfAnswer) => {
+const chooseAnswer = (list, indexOfAnswer) => {
     const items = list.querySelectorAll('li');
-    const timerEle = document.querySelector(".timer")
 
     items.forEach(item => {
-        item.addEventListener("click", function () {
+        item.onclick = function () {
             items.forEach(item => {
                 item.classList.remove("choosen-answer")
             })
 
-            ans = this;
+            userAnswer = this;
             this.classList.add("choosen-answer")
-<<<<<<< HEAD
             isChecked = true;
-
-=======
-            checked = true;
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
-            // checkAnswer(ans, indexOfAnswer, items, score)
-        })
-        // checkAnswer(ans, indexOfAnswer, items, score)
-        checked = false;
+        }
+        isChecked = false;
     })
+checkUserAnswer(indexOfAnswer,items)
+}
 
-<<<<<<< HEAD
-    checkAnsBtn.addEventListener('click', () => {
-        if (isChecked) {
-            let obj = {
-                ans: ans.dataset.index,
-                correctAns: allQuestions[currentIndex].index,
-=======
+const checkUserAnswer = (indexOfAnswer,items) => {
     checkAnsBtn.onclick = () => {
-        if (checked) {
-            const timerEle = document.querySelector(".timer")
+        if (isChecked) {
             const chosenAnswer = document.querySelector(".choosen-answer")
             if (chosenAnswer.dataset.index == indexOfAnswer) {
                 score += 5;
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
             }
-            undoStack.push(obj);
-            redoStack.length = 0;
-            clearInterval(time)
-<<<<<<< HEAD
-            if (ans.dataset.index == indexOfAnswer)
-                score += 5;
-
-            if (ans.dataset.index == indexOfAnswer && timerEle.innerHTML == 0)
+            if (chosenAnswer.dataset.index == indexOfAnswer && timerEle.innerHTML == 0) {
                 score -= 3;
-            checkChoosedAnswer(ans, indexOfAnswer, items )
-=======
+            }
+
+            allUserAnswers.push(chosenAnswer.dataset.index)
+            clearInterval(time)
             checkChoosedAnswer(chosenAnswer, indexOfAnswer, items)
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
             removeCheckBtn()
             isAnswered = true
 
@@ -202,28 +161,16 @@ const chooseAnswer = (allQuestions, list, indexOfAnswer) => {
         }
         else
             swal("Choose First", '', 'info')
-    })
+    }
 }
 
-// const checkAnswer = (answer, indexOfAnswer, items , score) => {
-//     checkAnsBtn.addEventListener('click', () => {
-//         if (checked) {
-//             clearInterval(time)
-//             checkChoosedAnswer(answer, indexOfAnswer, items, score)
-//             removeCheckBtn()   
-//         }
-//         else 
-//           swal("Choose First", '', 'info')
-//     })
-// }
 
 const startTimerForQuestion = () => {
     count = 31;
-    const timer = document.querySelector(".timer");
     time = setInterval(() => {
         setTimer = setTimeout(() => {
             count--;
-            timer.innerHTML = count;
+            timerEle.innerHTML = count;
             if (count == 0 && currentIndex <= 7) {
                 swal("Time Out", 'If you answer this question correctly, you will get only 2 points instead of 5', 'info')
                 clearTimeout(setTimer)
@@ -245,22 +192,25 @@ const getUserData = () => {
 }
 
 const setScoreInSortedPosition = (dataObj) => {
-    let index, dataList = [];
+    let index;
     for (let i = dataList.length - 1; i >= 0; i--)
         if (dataList[i].score > dataObj.score) {
             index = dataList.indexOf(dataList[i])
         }
     if (index != undefined) {
         dataList.splice(index, 0, dataObj)
-        displayLeaderboardData(dataList)
-        storeDataToLocalStorage(dataList)
+        displaySortedUserData(dataList)
     }
 
     else {
         dataList.push(dataObj)
-        displayLeaderboardData(dataList)
-        storeDataToLocalStorage(dataList)
+        displaySortedUserData(dataList)
     }
+}
+
+const displaySortedUserData =(dataList) => {
+    displayLeaderboardData(dataList)
+    storeDataToLocalStorage(dataList)
 }
 
 const displayLeaderboardData = (dataList) => {
@@ -305,10 +255,6 @@ const getNextQuestion = (allQuestions) => {
 }
 
 const getPrevQuestion = (allQuestions) => {
-<<<<<<< HEAD
-    console.log(isAnswered)
-=======
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
     if (!isAnswered) {
         swal("Not answered", "You need to answer first", "info")
         return
@@ -321,20 +267,15 @@ const getPrevQuestion = (allQuestions) => {
         displayElements(allQuestions, currentIndex)
         clearInterval(time)
         removeCheckBtn()
-<<<<<<< HEAD
-        const lastEle = undoStack.pop();
-        redoStack.push(lastEle);
-=======
->>>>>>> 7b296310f6ed19c39eb0be7a992ff958314ed6c4
     }
 }
 
-const initEventListeners = (allQuestions, list) => {
+const initEventListeners = (allQuestions) => {
     const nextQuestionBtn = document.querySelector(".next-question");
     const prevQuestionBtn = document.querySelector(".prev-question");
+
     nextQuestionBtn.addEventListener("click", () => { getNextQuestion(allQuestions) })
     prevQuestionBtn.addEventListener("click", () => { getPrevQuestion(allQuestions) })
     submitBtn.addEventListener("click", getUserData)
 }
-
 
